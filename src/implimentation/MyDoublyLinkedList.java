ae3e8808d9093eb9e3141e2844f0a9aa723b3c3e
PriyanshTri <Priyansh.tripathi_cs19@gla.ac.in>
2021-02-11 19:35:49 +0530

    my implimentation of doubly linked list

---
+++ src/implimentation/MyDoublyLinkedList.java
@@ -0,0 +1,25 @@
+package implimentation;
+
+import myinterface.DoublyLinkedListADT;
+
+public class MyDoublyLinkedList implements DoublyLinkedListADT {
+    private Node head;
+    private int size;
+    private Node previous;
+    public MyDoublyLinkedList(){
+        head=null;
+        previous=null;
+        size=0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return
+    }
+
+    @Override
+    public void addFirst(int element){
+        Node node=new Node(element);
+        if()
+    }
+}
---
+++ src/myinterface/DoublyLinkedListADT.java
@@ -0,0 +1,10 @@
+package myinterface;
+//this is adt of doubly linked list
+public interface DoublyLinkedListADT {
+    boolean isEmpty();
+    void addFirst(int element);
+//    void addLast(int element);
+//    boolean addAfterGivenElement(int element,int givenElement);
+//    boolean addBeforeGivenElement(int element,int givenElement);
+
+}
